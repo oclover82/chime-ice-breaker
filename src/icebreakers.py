fcd7fb5da6806f9d1a05cfa407c6ac3e642e9a35
oclover82 <120133898+oclover82@users.noreply.github.com>
2022-12-08 12:01:50 -0800

    Update icebreakers.py

--- src/icebreakers.py
+++ src/icebreakers.py
@@ -3,25 +3,25 @@
 import random
 
 DEFAULT_ICE_BREAKERS = [
-"Felicitations for the Season"
-"Sterling Carillon"
-"Circuitous Gambol of Festive Conifer"
-"Awesome Hibernal Acreage"
-"Altitudinous Celestials Acclaim"
-"Senior Flattened by a Cloven Aviator"
-"Covert Observation of Matriarch's Scandalous Osculation"
-"Petite Birthplace"
-"Sprightly Venerable Benefactor"
-"Allegiants Proceed"
-"Enquiry of Mutual Auditory Perception"
-"Hushed Darkness"
-"Noel - Envisage Blanched"
-"Inaugural Yule"
-"Royal Eastern Trio"
-"Planetary Jubilance"
-"Theurgical Cool Guy"
-"Matchless season"
-"Full-Grown Enumeration of Holiday Hopes"
+"Felicitations for the Season",
+"Sterling Carillon",
+"Circuitous Gambol of Festive Conifer",
+"Awesome Hibernal Acreage",
+"Altitudinous Celestials Acclaim",
+"Senior Flattened by a Cloven Aviator",
+"Covert Observation of Matriarch's Scandalous Osculation",
+"Petite Birthplace",
+"Sprightly Venerable Benefactor",
+"Allegiants Proceed",
+"Enquiry of Mutual Auditory Perception",
+"Hushed Darkness",
+"Noel - Envisage Blanched",
+"Inaugural Yule",
+"Royal Eastern Trio",
+"Planetary Jubilance",
+"Theurgical Cool Guy",
+"Matchless season",
+"Full-Grown Enumeration of Holiday Hopes",
 "Commencement of Yuletide Complexion"
 ]
 
